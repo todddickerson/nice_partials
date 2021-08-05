52a1941128f25cdc1646b2ae0a8384eb5caa8d7b
Todd Dickerson <todd@spontent.com>
2021-08-05 15:37:50 -0400

    Attempting to use in other renderers

--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -21,3 +21,25 @@ class ActionView::PartialRenderer
     return result
   end
 end
+
+class ActionView::Renderer
+  alias_method :original_render, :render
+
+  # See `content_for` in `lib/nice_partials/partial.rb` for something similar.
+  def render(context, options, block)
+    if block
+      partial_prefix = nice_partials_locale_prefix_from_view_context_and_block(context, block)
+      context.nice_partials_push_t_prefix partial_prefix
+    else
+      # Render partial calls with no block should disable any prefix magic.
+      context.nice_partials_push_t_prefix ''
+    end
+
+    result = original_render(context, options, block)
+
+    # Whether there was a block or not, pop off whatever we put on the stack.
+    context.nice_partials_pop_t_prefix
+
+    return result
+  end
+end
